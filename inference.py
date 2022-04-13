2411184ad7089f8cae415bbbfb9a9b1e022c2e9d
Praneeth <praneethcruzon@gmail.com>
2022-04-13 20:12:12 +0530

    added inference script to stack images and run CNN inference

---
+++ inference.py
@@ -0,0 +1,38 @@
+from configparser import Interpolation
+import cv2
+import torch
+from torchvision import transforms
+from torchvision.utils import save_image
+from architecture import CNN_LSTM
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+image = cv2.imread("./data/image.jpg")
+image_2 = cv2.imread("./data/image1.jpg")
+
+image = cv2.resize(image, (1280, 348), interpolation = cv2.INTER_LINEAR)
+image_2 = cv2.resize(image_2, (1280,348), interpolation = cv2.INTER_LINEAR)
+
+transform = transforms.ToTensor()
+
+image = transform(image)
+# image = image.unsqueeze(0)
+
+image_2 = transform(image_2)
+# image_2 = image_2.unsqueeze(0)
+
+stacked_image = torch.stack(
+    (image, image_2),
+    dim = 0
+)
+
+stacked_image = stacked_image.to(device)
+
+cnn_lstm = CNN_LSTM()
+
+cnn_lstm.eval()
+cnn_lstm = cnn_lstm.to(device)
+
+print(stacked_image.shape)
+
+x = cnn_lstm(stacked_image)
